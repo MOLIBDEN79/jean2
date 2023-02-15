3005614cea291ba68cee1a0b040638b0a5bdf4bc
BiJean <Akjan601@gmail.com>
2023-02-15 17:24:26 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+a = 5
+b = 6
+result = 5 == 6  # сохраняем результат операции в переменную
+print(result)  # False - 5 не равно 6
+print(a != b)  # True
+print(a > b)  # False - 5 меньше 6
+print(a < b)  # True
+
+bool1 = True
+bool2 = False
+print(bool1 == bool2)  # False - bool1 не равно bool2
+
+isMarried = False
+print(isMarried)  # False
+
+isAlive = True
+print(isAlive)  # True
+
+height = 1.68
+pi = 3.14
+weight = 68.
+print(height)   # 1.68
+print(pi)       # 3.14
+print(weight)   # 68.0
+
+print(6 + 2)  # 8
+print(6 - 2)  # 4
+print(6 * 2)  # 12
+print(6 / 2)  # 3.0
+print(7 / 2)  # 3.5
+print(7 // 2)  # 3
+print(6 ** 2)  # Возводим число 6 в степень 2. Результат - 36
+print(7 % 2)  # Получение остатка от деления числа 7 на 2. Результат - 1
+
+number = 10
+number += 5
+print(number)  # 15
+
+number -= 3
+print(number)  # 12
+
+number *= 4
+print(number)  # 48
